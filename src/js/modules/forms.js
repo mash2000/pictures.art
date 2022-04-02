d9bc64f5e1f38bab26002f3999c3eb36eb994bbf
kamshir <iammaksim2000shirshikov@gmail.com>
2022-04-02 15:56:35 +0300

    upload from server

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,4 +1,5 @@
 // import checkNumInputs from "./checkNumInputs"; 
+import { postData } from "../services/requests";
 
 const forms = (state) => {
   const form  = document.querySelectorAll('form'),
@@ -19,16 +20,6 @@ const forms = (state) => {
   const path = {
     designer: 'assets/server.php',
     question: 'assets/question.php'
-  };
-
-  const postData = async (url, data) => {
-    document.querySelector('.status').textContent = message.loading;
-    let res = await fetch(url, {
-      method: "POST",
-      body: data
-    });
-
-    return await res.text();
   };
 
   const clearInputs = () => {
