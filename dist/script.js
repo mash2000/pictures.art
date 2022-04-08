1c56a8d5102c4bb544416f2df07c54db6a41e312
Ширшиков Максим <Iammaksim2000shirshikov@gmail.com>
2022-04-08 17:56:39 +0300

    delete old code

--- dist/script.js
+++ dist/script.js
@@ -4466,20 +4466,7 @@ var accordion = function accordion(triggersSelector, itemsSelector) {
         this.nextElementSibling.style.maxHeight = '0px';
       }
     });
-  }); // const blocks = document.querySelectorAll(itemsSelector);
-  // btns.forEach(btn => {
-  // 		btn.addEventListener('click', function() {
-  // 				if (!this.classList.contains('active')) {
-  // 						btns.forEach(btn => {
-  // 								btn.classList.remove('active', 'active-style');
-  // 						});
-  // 						this.classList.add('active', 'active-style');
-  // 				}
-  // 		});
-  // });
-  // blocks.forEach(block => {
-  // 		block.classList.add('animated', 'fadeInDown');
-  // });
+  });
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (accordion);
--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -13,21 +13,6 @@ const accordion = (triggersSelector, itemsSelector) => {
 					}
 			});
 	});
-
-	// const blocks = document.querySelectorAll(itemsSelector);
-	// btns.forEach(btn => {
-	// 		btn.addEventListener('click', function() {
-	// 				if (!this.classList.contains('active')) {
-	// 						btns.forEach(btn => {
-	// 								btn.classList.remove('active', 'active-style');
-	// 						});
-	// 						this.classList.add('active', 'active-style');
-	// 				}
-	// 		});
-	// });
-	// blocks.forEach(block => {
-	// 		block.classList.add('animated', 'fadeInDown');
-	// });
 };
 
 export default accordion;
