cb49129fe94bdd7ed24be1929e54a19d2c17d82c
Ширшиков Максим <Iammaksim2000shirshikov@gmail.com>
2022-04-08 18:09:10 +0300

    edit burger menu

--- src/js/modules/burger.js
+++ src/js/modules/burger.js
@@ -11,6 +11,12 @@ const burger = (menuSelector, burgerSelector) => {
 			menuElem.style.display = 'none';
 		}
 	});
+
+	window.addEventListener('resize', () => {
+		if (window.screen.availWidth > 992){
+			menuElem.style.display = 'none';
+		}
+	});
 };
 
 export default burger;
